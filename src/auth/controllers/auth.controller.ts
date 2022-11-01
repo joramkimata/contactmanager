dc59a2e3055f9be277cf91836035e6ad8d6d2e1c
dev1 <dev1@example.com>
2022-11-01 08:21:39 +0300

    add more codes

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,51 +1,51 @@
-import { JwtAuthGuard } from './../guards/jwt.guard';
-import { LoginDto } from './../dtos/login.dto';
-import { Body, Controller, Get, HttpStatus, Post, Req, UseGuards } from "@nestjs/common";
-import { AuthService } from '../services/auth.service';
-import { Request } from 'express';
-import { GetUser } from '../decorators/get-user.decorator';
-import { User } from 'src/users/entities/users.entity';
-
+import { JwtAuthGuard } from "./../guards/jwt.guard";
+import { LoginDto } from "./../dtos/login.dto";
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, Req, UseGuards } from "@nestjs/common";
+import { AuthService } from "../services/auth.service";
+import { Request } from "express";
+import { GetUser } from "../decorators/get-user.decorator";
+import { User } from "src/users/entities/users.entity";
 
 
 @Controller("/auth")
 export class AuthController {
 
-    constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService) {
+  }
 
 
-    @Post("/login")
-    @HttpStatus(200)
-    login(
-        @Body()
-        loginDto: LoginDto
-    ) {
-        return this.authService.login(loginDto);
-    }
+  @Post("/login")
+  @HttpCode(200)
+  login(
+    @Body()
+      loginDto: LoginDto
+  ) {
+    return this.authService.login(loginDto);
+  }
 
-    @Get("/profile")
-    @UseGuards(JwtAuthGuard)
-    profile(
-        // @Req() res: Request
-        @GetUser() user: User
-    ) {
-        user.password = undefined;
-        return user;
-    }
+  @Get("/profile")
+  @UseGuards(JwtAuthGuard)
+  profile(
+    // @Req() res: Request
+    @GetUser() user: User
+  ) {
+    user.password = undefined;
+    return user;
+  }
 
-    @Get('/logout')
-    @UseGuards(JwtAuthGuard)
-    logout(
-        @GetUser() user: User
-    ) {
-        return this.authService.logout(user);
-    }
+  @Get("/logout")
+  @UseGuards(JwtAuthGuard)
+  logout(
+    @GetUser() user: User
+  ) {
+    return this.authService.logout(user);
+  }
 
-    @Post('/refresh')
-    @HttpStatus(200)
-    refresh(
-        @Body('refreshToken') refreshToken: string
-    ) {
-        return this.authService.refresh(refreshToken);
-    }
+  @Post("/refresh")
+  @HttpCode(200)
+  refresh(
+    @Body("refreshToken") refreshToken: string
+  ) {
+    return this.authService.refresh(refreshToken);
+  }
 }

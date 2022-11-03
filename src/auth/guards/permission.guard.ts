ab81f7e1eb57986f005e4fd09daeb432f210d72f
dev1 <dev1@example.com>
2022-11-03 12:59:05 +0300

    add fix

--- src/auth/guards/permission.guard.ts
+++ src/auth/guards/permission.guard.ts
@@ -1,6 +1,6 @@
 import {
-    CanActivate, ExecutionContext, ForbiddenException, Injectable,
-    UnauthorizedException
+  CanActivate, ExecutionContext, ForbiddenException, Injectable,
+  UnauthorizedException
 } from "@nestjs/common";
 import { Reflector } from "@nestjs/core";
 import { GqlExecutionContext } from "@nestjs/graphql";
@@ -12,45 +12,45 @@ import { IPermission } from "../../users/decorators/has-permission.decorator";
 @Injectable()
 export class PermissionGuard implements CanActivate {
 
-    constructor(
-        private authService: AuthService,
-        private reflector: Reflector
-    ) { }
+  constructor(
+    private authService: AuthService,
+    private reflector: Reflector
+  ) {
+  }
 
-    async canActivate(context: ExecutionContext) {
+  async canActivate(context: ExecutionContext) {
 
-        // Logic
-        const ctx = GqlExecutionContext.create(context);
+    // Logic
+    const ctx = GqlExecutionContext.create(context);
 
-        const { user } = ctx.getContext().req;
+    const { user } = ctx.getContext().req;
 
-        if (!user) {
-            throw new UnauthorizedException('Invalid User');
-        }
-
-        const accessMethod: IPermission = this.reflector.get<IPermission>('permission', context.getHandler());
-
-        const accessClass:IPermission = this.reflector.get<IPermission>('permission', context.getClass());
-
-        if (accessMethod) {
-            const permsArray = await this.authService.getUserPermissions(user.username);
-
-            if (!permsArray.some(p => p === accessMethod.name)) {
-                throw new ForbiddenException('Access Denied');
-            }
-        }
-
-        if (accessClass) {
-            const permsArray = await this.authService.getUserPermissions(user.username);
-
-            if (!permsArray.some(p => p === accessClass.name)) {
-                throw new ForbiddenException('Access Denied');
-            }
-        }
-
-
-
-        return true;
+    if (!user) {
+      throw new UnauthorizedException("Invalid User");
     }
+
+    const accessMethod: IPermission = this.reflector.get<IPermission>("permission", context.getHandler());
+
+    const accessClass: IPermission = this.reflector.get<IPermission>("permission", context.getClass());
+
+    if (accessMethod) {
+      const permsArray = await this.authService.getUserPermissions(user.username);
+
+      if (!permsArray.some(p => p === accessMethod.name)) {
+        throw new ForbiddenException("Access Denied");
+      }
+    }
+
+    if (accessClass) {
+      const permsArray = await this.authService.getUserPermissions(user.username);
+
+      if (!permsArray.some(p => p === accessClass.name)) {
+        throw new ForbiddenException("Access Denied");
+      }
+    }
+
+
+    return true;
+  }
 
 }

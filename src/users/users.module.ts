72cc5c14f6e96bd2eb28733b583b14158b025a95
dev1 <dev1@example.com>
2022-11-07 08:17:41 +0300

    add perms

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,45 +1,48 @@
-import { AuthModule } from 'src/auth/auth.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from "src/auth/auth.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { PublicUserResolver, UserResolver } from "./resolvers/user.resolver";
-import { Module, OnModuleInit } from '@nestjs/common';
-import { UserService } from './services/user.service';
-import { User } from './entities/users.entity';
-import { RoleResolver } from './resolvers/role.resolver';
-import { RoleService } from './services/role.service';
-import { Role } from './entities/role.entity';
-import { PermissionService } from './services/permission.service';
-import { Permission } from './entities/permission.entity';
-import { PermissionResolver } from './resolvers/permission.resolver';
-import { DiscoveryModule, DiscoveryService } from '@golevelup/nestjs-discovery';
+import { Module, OnModuleInit } from "@nestjs/common";
+import { UserService } from "./services/user.service";
+import { User } from "./entities/users.entity";
+import { RoleResolver } from "./resolvers/role.resolver";
+import { RoleService } from "./services/role.service";
+import { Role } from "./entities/role.entity";
+import { PermissionService } from "./services/permission.service";
+import { Permission } from "./entities/permission.entity";
+import { PermissionResolver } from "./resolvers/permission.resolver";
+import { DiscoveryModule, DiscoveryService } from "@golevelup/nestjs-discovery";
+import { DashboardResolver } from "../dashboard/resolvers/dashboard.resolver";
 
 @Module({
-    imports: [
-        DiscoveryModule,
-        AuthModule,
-        TypeOrmModule.forFeature([
-            User,
-            Role,
-            Permission
-        ])
-    ],
-    providers: [
-        UserResolver,
-        UserService,
-        RoleResolver,
-        RoleService,
-        PermissionService,
-        PermissionResolver,
-        PublicUserResolver
-    ]
+  imports: [
+    DiscoveryModule,
+    AuthModule,
+    TypeOrmModule.forFeature([
+      User,
+      Role,
+      Permission
+    ])
+  ],
+  providers: [
+    UserResolver,
+    UserService,
+    RoleResolver,
+    RoleService,
+    PermissionService,
+    PermissionResolver,
+    PublicUserResolver,
+    DashboardResolver
+  ]
 })
 export class UsersModule implements OnModuleInit {
 
-    constructor(private permissionService: PermissionService,
-        private userService: UserService,
-        private discoveryService: DiscoveryService) { }
+  constructor(private permissionService: PermissionService,
+              private userService: UserService,
+              private discoveryService: DiscoveryService) {
+  }
 
-    async onModuleInit() {
-        await this.permissionService.seedPermissions(this.discoveryService);
-        await this.userService.seedAdmin();
-    }
+  async onModuleInit() {
+    await this.permissionService.seedPermissions(this.discoveryService);
+    await this.userService.seedAdmin();
+  }
 }

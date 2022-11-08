ee5d33ae8579bc7a827174cf072c4e89a10a7cc3
dev1 <dev1@example.com>
2022-11-08 08:26:58 +0300

    dashboard fix

--- src/dashboard/dashboard.module.ts
+++ src/dashboard/dashboard.module.ts
@@ -3,13 +3,15 @@ import { DashboardService } from "./services/dashboard.service";
 import { DashboardResolver } from "./resolvers/dashboard.resolver";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { User } from "../users/entities/users.entity";
+import { AuthModule } from "../auth/auth.module";
 
 @Module({
   providers: [DashboardService, DashboardResolver],
   imports: [
     TypeOrmModule.forFeature([
       User
-    ])
+    ]),
+    AuthModule
   ]
 })
 export class DashboardModule {
